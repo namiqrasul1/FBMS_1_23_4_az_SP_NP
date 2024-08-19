642ec62874ae2e06e6d55dbcc544ec42718375fa
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-08-19 11:19:07 +0400

    Lesson 1. Instro, Processes

---
+++ System Programming/Lesson1SPIntroProcesses/Lesson1SPIntroProcesses/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+//void foo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("foo()");
+//        Thread.Sleep(200);
+//    }
+//}
+
+//void boo()
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("boo()");
+//        Thread.Sleep(200);
+//    }
+//}
+
+
+//var thread = new Thread(boo);
+
+//thread.Start();
+//foo();
+
+//foreach (var arg in args)
+//{
+//    Console.WriteLine(arg);
+//}
+
+//Process.Start("mspaint");
+//Process.Start("notepad");
+//Process.Start("cmd");
+//Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", "https://www.google.com");
+
+
+//var paint = Process.Start("mspaint");
+
+//Thread.Sleep(2000);
+
+//paint.Kill();
+
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+//Process.Start("mspaint");
+
+//var paints = Process.GetProcessesByName("mspaint");
+
+//foreach (var p in paints)
+//{
+//    Console.WriteLine($"{p.Id} {p.ProcessName} {p.MachineName} {p.Threads.Count}");
+//    p.Kill();
+//}
+
+
+//var paints = Process.GetProcesses();
+
+//foreach (var p in paints)
+//{
+//    Console.WriteLine($"{p.Id} {p.ProcessName} {p.MachineName} {p.Threads.Count}");
+//    //p.Kill();
+//}
+
+//Process.Start("chrome");
+
+//var psi = new ProcessStartInfo();
+//psi.FileName = "notepad";
+//psi.Arguments = "hakuna";
+
+//Process.Start(psi);
