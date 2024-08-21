068e04011a460cf394d4a333f3ddf63053c1b3bb
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-08-21 11:38:04 +0400

    Lesson 2. AppDomain, Threads

---
+++ System Programming/Lesson2AppDomain/Lesson2AppDomain/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Security;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace Lesson2AppDomain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var domain = AppDomain.CreateDomain("BabatDomain");
+
+            //var type = typeof(Virus);
+            ////Console.WriteLine(type.FullName);
+            ////Console.WriteLine(type.Name);
+            ////Console.WriteLine(type.Assembly.FullName);
+
+            //var obj = domain.CreateInstance(type.Assembly.FullName, type.FullName);
+
+            //AppDomain.Unload(domain);
+            //Console.ReadLine();
+
+
+            //PermissionSet perm = new PermissionSet(System.Security.Permissions.PermissionState.None);
+
+            //perm.AddPermission(new FileIOPermission(PermissionState.None));
+            //perm.AddPermission(new FileIOPermission(FileIOPermissionAccess.NoAccess, "C://"));
+
+            //var appDomainSetup = new AppDomainSetup();
+            //appDomainSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+            //AppDomain appDomain = AppDomain.CreateDomain("BabatDomain", null, appDomainSetup, perm);
+
+            //AppDomain.Unload(appDomain);
+
+
+            Thread t = new Thread(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.WriteLine('x');
+                    Thread.Sleep(100);
+                }
+            });
+
+            t.Start();
+
+
+            Console.ReadKey();
+            t.Suspend();
+            Console.ReadKey();
+            t.Resume();
+
+            Console.ReadKey();
+            t.Abort();
+        }
+    }
+
+    class Virus
+    {
+        public Virus()
+        {
+            Console.WriteLine("Salam, men hackerem, geldim ogurluga");
+        }
+
+        ~Virus()
+        {
+            Console.WriteLine("Sagol, men ogurladim gedirem");
+        }
+    }
+}
---
+++ System Programming/Lesson2AppDomainThread/Lesson2AppDomainThread/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel.Design;
+using System.Threading.Channels;
+
+internal class Program
+{
+    static void SomeMethod()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write('x');
+        }
+        Console.WriteLine();
+        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {Thread.CurrentThread.Name}");
+    }
+
+    static void SomeMethod1()
+    {
+
+        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {Thread.CurrentThread.Name}");
+
+        Console.WriteLine("Thread Started");
+        Thread.Sleep(1000);
+        Console.WriteLine("Thread Finished");
+    }
+
+    static void Foo(int i) { Console.WriteLine(i * 19); }
+
+    static void SomeMethodWithParams(object? obj)
+    {
+        Console.WriteLine(obj);
+    }
+
+    private static void Main(string[] args)
+    {
+        //Thread thread = new(SomeMethod);
+        //thread.Name = "kamil";
+        ////thread.Priority = ThreadPriority.Highest;
+        ////thread.Priority = ThreadPriority.Lowest;
+
+        //thread.Start();
+
+        //for (int i = 0; i < 1000; i++)
+        //{
+        //    Console.Write('y');
+        //}
+
+        //Thread.Sleep(2000);
+        //Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {Thread.CurrentThread.Name}");
+
+        //var thread = new Thread(SomeMethod1);
+        //thread.IsBackground = true;
+        //thread.Start();
+        ////Thread.Sleep(2000);
+        //Console.Write('d');
+
+
+        //Thread thread = new(() =>
+        //{
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        Console.WriteLine("salam");
+        //    }
+        //});
+
+        //thread.Start();
+
+
+        //Thread th = new(SomeMethodWithParams);
+        //th.Start("salam");
+
+        //Thread th = new(obj => Console.WriteLine(obj));
+        //th.Start("salam");
+
+        //Thread th = new Thread(() =>
+        //{
+        //    Foo(6);
+        //});
+
+        //th.Start();
+
+
+        //for(int i = 0; i < 10; i++)
+        //{
+        //    int tt = i;
+        //    Thread t = new(() => Console.WriteLine(tt));
+        //    t.Start();
+        //}
+
+        //var str = "Miro";
+
+        //Thread t = new(() => Console.WriteLine(str));
+
+        //str = "Talo";
+        //Thread t1 = new(() => Console.WriteLine(str));
+
+        //t.Start();
+        //t1.Start();
+
+
+        Thread t = new(() =>
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine('x');
+                Thread.Sleep(100);
+            }
+        });
+
+        t.Start();
+
+        Console.WriteLine("0000000000000");
+
+        t.Join();
+
+        Console.WriteLine("Hello");
+    }
+}
