3ebceeb0eab54fe38ce3dbbb9989ef96bce30a16
Rasullu Namig Bahman <namiqrasullu@itstep.edu.az>
2024-09-11 11:15:32 +0400

    Lesson 2. TCP v2

---
+++ Network Programming/Lesson2TCP/ClientSide/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var client = new TcpClient();
+
+var port = 27001;
+var ip = IPAddress.Parse("10.1.18.1");
+var ep = new IPEndPoint(ip, port);
+
+Console.ReadKey();
+try
+{
+	client.Connect(ep);
+	if (client.Connected)
+	{
+		while (true)
+		{
+			var text = Console.ReadLine();
+			var sw = new StreamWriter(client.GetStream());
+			sw.WriteLine(text);
+			sw.AutoFlush = true;
+		}
+	}
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ Network Programming/Lesson2TCP/ServerSide/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var port = 27001;
+var ip = IPAddress.Parse("10.1.18.1");
+var ep = new IPEndPoint(ip, port);
+
+using var listener = new TcpListener(ep);
+listener.Start();
+Console.WriteLine($"Listening on: {listener.LocalEndpoint}");
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    var task = Task.Run(() =>
+    {
+        Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+        var stream = client.GetStream();
+        var sr = new StreamReader(stream);
+        while (true)
+        {
+            var message = sr.ReadLine();
+            Console.WriteLine($"{client.Client.RemoteEndPoint}: {message}");
+        }
+    });
+}
